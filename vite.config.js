c10cca983afc5c486947305c54749d98db46c9cf
Yuliya-1102 <bambina-kissi@yandex.ru>
2024-08-07 10:47:52 +0300

    t

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
+const filesHtml = {
+    main: resolve(__dirname, 'index.html'),
+    pages: resolve(__dirname, 'pages/index.html'),
+};
+
+// array.forEach(element => {
+
+// });
+
+
+
 export default defineConfig({
     build: {
         rollupOptions: {
             output: {
                 dir: './dist/',
-                entryFileNames: 'app-editor-desiner.js',
-                assetFileNames: 'style.css',
+                entryFileNames: 'script-new.js',
+                assetFileNames: 'style-new.css',
                 manualChunks: undefined,
-            }
+            },
+            input: filesHtml
         }
     },
     plugins: [injectHTML()],
